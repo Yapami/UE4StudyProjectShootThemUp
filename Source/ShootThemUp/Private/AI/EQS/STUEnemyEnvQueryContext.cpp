e52b7da4e97faa8174c8cac893870060a468dbb8
Yapami <vladyapami@gmail.com>
2023-01-21 20:59:21 +0200

    BU after lesson 104

---
+++ Source/ShootThemUp/Private/AI/EQS/STUEnemyEnvQueryContext.cpp
@@ -0,0 +1,23 @@
+// Slava Ukraine
+
+
+#include "AI/EQS/STUEnemyEnvQueryContext.h"
+#include "EnvironmentQuery/EnvQueryTypes.h"
+#include "EnvironmentQuery/Items/EnvQueryItemType_Actor.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
+
+void USTUEnemyEnvQueryContext::ProvideContext(FEnvQueryInstance& QueryInstance,
+    FEnvQueryContextData& ContextData) const
+{
+    const auto QueryOwner = Cast<AActor>(QueryInstance.Owner.Get());
+
+    const auto BlackBoard = UAIBlueprintHelperLibrary::GetBlackboard(QueryOwner);
+    if(!BlackBoard)
+    {
+        return;
+    }
+    const auto ContextActor = BlackBoard->GetValueAsObject(EnemyActorKeyName);
+    
+    UEnvQueryItemType_Actor::SetContextHelper(ContextData, Cast<AActor>(ContextActor));
+}
