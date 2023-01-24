06dcc071020d6bef58c0f5b1bd50542785fd5e3e
Yapami <vladyapami@gmail.com>
2023-01-24 21:34:34 +0200

    BU after lesson 111

--- Source/ShootThemUp/Public/STUGameModeBase.h
+++ Source/ShootThemUp/Public/STUGameModeBase.h
@@ -4,7 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "STUCoreTypes.h"
 #include "STUGameModeBase.generated.h"
+
+class AAIController;
 
 /**
  *
@@ -16,4 +19,20 @@ class SHOOTTHEMUP_API ASTUGameModeBase : public AGameModeBase
 
 public:
     ASTUGameModeBase();
+
+    virtual void StartPlay() override;
+
+    virtual UClass* GetDefaultPawnClassForController_Implementation(AController* InController) override;
+protected:
+    UPROPERTY(EditDefaultsOnly, Category = "Game")
+    TSubclassOf<AAIController> AIControllerClass;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Game")
+    TSubclassOf<APawn> AIPawnClass;
+    
+    UPROPERTY(EditDefaultsOnly, Category = "Game")
+    FGameData GameData;
+
+private:
+    void SpawnBots();
 };
